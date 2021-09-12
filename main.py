0e5cdaab96acc5797a534b5ede0cc430abf41c3a
Onawahya Jones <wolfmaw@gmail.com>
2021-09-11 20:04:25 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Color, Button, Icon, Side, Port, Direction
+from pybricks.tools import wait
+from pybricks.geometry import Axis
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.robotics import DriveBase
+
+Robot = PrimeHub()
