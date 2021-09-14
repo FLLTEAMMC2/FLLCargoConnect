1d62db79604238781dc250a543c7461aa6a59e86
Coach Ona <wolfmaw@gmail.com>
2021-09-13 20:07:15 -0700

    Stub entries for utility.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from pybricks.hubs import PrimeHub
-from pybricks.parameters import Color, Button, Icon, Side, Port, Direction
+from pybricks.parameters import Color, Button, Side, Port, Direction
 from pybricks.tools import wait
 from pybricks.geometry import Axis
 from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.robotics import DriveBase
+import utility
 
+
+# STATE 1 Initialize global variables
 Robot = PrimeHub()
+
+utility.MoveStraight()
+
+
+# STATE 2 Start monitor loop
+# Check for button presses, increment selection number, start selection program
---
+++ utility.py
@@ -0,0 +1,6 @@
+# enter code for move straight
+def MoveStraight():
+
+
+# enter code to rotate the robot using both wheels
+def RotateByDegrees():
