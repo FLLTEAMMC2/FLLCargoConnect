8c5e3c746d711df0f1ce86113c781fca696855d0
fatherchaos <wolfmaw@gmail.com>
2021-09-14 11:08:21 -0700

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,6 +1,15 @@
+# This file contains all the movement and control instructions.
+# By putting the commands here, everyone can use the same command and have the same behavior on the robot.
+
+# Some basic ideas are listed below
 # enter code for move straight
 def MoveStraight():
 
-
 # enter code to rotate the robot using both wheels
 def RotateByDegrees():
+  
+def RotateFirstAccessory():
+  
+def FollowLineForDistanceWithRightSensor():
+  
+  
