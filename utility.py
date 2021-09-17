4bd7de9b41488e13625dfad1c4ece0d42deee8ae
Calebndale <89821446+Calebndale@users.noreply.github.com>
2021-09-16 21:13:44 -0700

    Update utility.py
    Add line_follow_until_color function and sample function call

--- utility.py
+++ utility.py
@@ -13,3 +13,31 @@ def RotateFirstAccessory():
 def FollowLineForDistanceWithRightSensor():
   
   
+def line_follow_until_color(line_sensor_right, line_sensor_left, drive_speed, proportional_gain, stop_color):
+    # Calculate the light threshold. Choose values based on your measurements.
+    BLACK = 10
+    WHITE = 95
+    threshold = (BLACK + WHITE) / 2
+
+    while line_sensor_right.color() != stop_color:
+        #print(line_sensor.reflection(), line_sensor.color(), line_sensor.hsv())
+        # Calculate the deviation from the threshold.
+        deviation = line_sensor_left.reflection() - threshold
+
+        # Calculate the turn rate.
+        turn_rate = proportional_gain * deviation
+
+        # Set the drive base speed and turn rate.
+        robot.drive(drive_speed, turn_rate)
+
+        # You can wait for a short time or do other things in this loop.
+        wait(10)
+
+line_follow_until_color(
+    line_sensor_right = ColorSensor(Port.F), 
+    line_sensor_left = ColorSensor(Port.E), 
+    drive_speed = 150,
+    proportional_gain = 1.2,
+    stop_color=Color.RED)
+        
+  
